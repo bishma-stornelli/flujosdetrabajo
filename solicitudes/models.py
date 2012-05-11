b6f0ee58bb525f761f96106140c214b0c2536119
Samuel Bártoli <smbartoli@gmail.com>
2012-05-10 22:34:52 -0430

    implementado entidades y relaciones
    entidades: respuesta, solicitud, registro
    relaciones: solicita, participa, realiza, responde

--- solicitudes/models.py
+++ solicitudes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Solicitud(models.Model):
+    idSOLICITUD = models.IntegerField()
+    idFlujo = models.ForeignKey(Flujo)
+    solicitante = models.ForeignKey(Solicitante)
+    fechaSolicitud = models.DateTimeField("Date published")
+
+class Solicita(models.Model):
+    usuarios = models.ManyToMany(Usuario)
+    idSolicitudes = models.ManyToMany(Solicitud)
+
+class Realiza(models.Model):    
+    estado = models.IntegerField()
+    idSolicitudes = models.ManyToMany(Solicitud,through='REGISTRO')
+    idPasos = models.ManyToMany(PASO,through='REGISTRO')
+
+class Respuesta(models.Model):
+    valor = models.CharField( max_length = 20)
+
+class Registro(models.Model):
+    fechaEntrada = models.DateField(_("Fecha de entrada"), default = datetime.date.today)
+    fechaSalida = models.CharField( max_length = 15)
+    estado = models.CharField( max_length = 20)
+ 
+class Participa(models.Model)
+    solp = models.ForeignKey(Flujo)
+
+class Responde(models.Model):
+    idSolicitudes = models.ManyToManyField(Solicitud)
+    nombres = models.ManyToManyField(Campo)
+
+    
