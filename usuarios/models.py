f6c0f66898c5215fdb6c0c2cf290d74091c411be
ale7714 <aept7714@gmail.com>
2012-05-10 23:42:00 -0430

    Agregada la clases de Alerta, Informe, Plantilla, Usuario... Faltan metodos

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
 
-# Create your models here.
+class ALERTA(models.Model):
+    responsable = models.IntegerField()
+    tipo =  models.IntegerField()
+    descripcion = models.TextField()
+    llegada =  models.BooleanField()
+    
+class INFORME(models.Model):
+    descripcion = models.TextField()
+    responsable = models.IntegerField()
+    
+class PLANTILLA(models.Model):
+    formato =  models.CharField(max_length=30)
+    
+class PERFIL(User):
+    dni = models.CharField(max_length=30)
+    objects = UserManager() 
+    
+ class PRODUCEALERTA(models.Model):
+    idPaso = models.ForeignKey(PASO)
+    idAlerta = models.ForeignKey(ALERTA)
+
+class GENERA(models.Model):
+    idPaso = models.ForeignKey(PASO)
+    idInforme = models.ForeignKey(INFORME)
+
+class IBASADAEN(models.Model):
+    idInforme = models.ForeignKey(INFORME)
+    idPlantilla = models.ForeignKey(PLANTILLA)
+    
+class ABASADAEN(models.Model):
+    idAlerta = models.ForeignKey(ALERTA)
+    idPlantilla = models.ForeignKey(PLANTILLA)
