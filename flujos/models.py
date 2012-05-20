90ac665ffcf6ccd2edaa11251cfbba4657e4bed5
flujos <alemartini2204@gmail.com>
2012-05-20 15:22:45 -0430

    actualizado modelo de flujos

--- flujos/models.py
+++ flujos/models.py
@@ -13,7 +13,6 @@ class Alerta(models.Model):
     solicitante_es_receptor = models.BooleanField()
     tipos = models.ManyToManyField('TipoAlerta', related_name='alertas')
 
-
 class TipoAlerta(models.Model):
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=256)
@@ -31,3 +30,40 @@ class Informe(models.Model):
    
 class Plantilla(models.Model):
     formato = models.TextField()
+	
+class Campo(models.Model):
+    paso = models.ForeignKey(Paso)
+    criterio = models.ForeignKey(Criterio)
+    planilla = models.ForeignKey(Plantilla)
+    solicitud = models.ForeignKey('solicitudes.models.Solicitud')
+	nombre = models.CharField(max_length=30)
+    responsable = models.IntegerField()
+    tipo = models.IntegerField()
+    esObligatorio = models.BooleanField()
+
+class Criterio(models.Model):
+    campo = models.ForeignKey(Campo)
+    descripcion = models.TextField()
+    expresion = models.CharField(max_length=30)
+	
+class Paso(models.Model):	
+	flujo = models.ForeignKey()
+	solicitud = models.ForeignKey()
+    nombre = models.CharField(max_length=30)
+    tipo = models.IntegerField(
+    descripcion = models.TextField()
+	alertas = models.ManyToManyField(Alerta)
+	informes = models.ManyToManyField(Informe)
+	campos = models.ManyToManyField(Campo)
+
+class Flujo(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=30)
+    unidad = models.ForeignKey('unidades.models.Unidad')
+	solicitud = models.ManyToManyField('solicitudes.models.Solicitud')
+	paso = models.ManyToManyField(Paso)
+	
+class Camino(models.Model):
+	paso_origen = models.ManyToManyField(Paso, through = 'Criterio')
+	paso_destino = models.ManyToManyField(Paso)
