d133ee41308f464d09e20a596a831fb3ed98e70f
cesar <cesar@cesar-OEM.(none)>
2012-05-30 23:22:48 -0430

    cacion hecha al admin.py de usuario

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 from usuarios.models import *
-=======
->>>>>>> ee5b9b6321ad9a9111263c94cdf05c6b56330724
-from django.contrib import admin
 from unidades.models import SolicitudPrivilegio, Unidad
-
-<<<<<<< HEAD
+from django.contrib import admin
 
 admin.site.register(PerfilDeUsuario)
-=======
 admin.site.register(Unidad)
 admin.site.register(SolicitudPrivilegio)
->>>>>>> ee5b9b6321ad9a9111263c94cdf05c6b56330724
+
