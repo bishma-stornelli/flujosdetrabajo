778d8e50ce43a075fc96015eb1046da3436fd4cc
César Freitas <asgardss88@gmail.com>
2012-06-02 16:48:18 -0430

    1)Eliminadas urls centralizadas. 2)Modulo de usuarios adaptado a las convenciones

--- flujosdetrabajo/urls.py
+++ flujosdetrabajo/urls.py
@@ -12,15 +12,12 @@ urlpatterns = patterns('',
     # url(r'^flujosdetrabajo/', include('flujosdetrabajo.foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'usuarios.views.index'),
-    url(r'^index$', 'usuarios.views.index'),
-    url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'log_in.html'}),
-    url(r'^logout/', 'usuarios.views.log_out'),
-    url(r'^registro$', 'usuarios.views.registro'),
+    url(r'^index/$', 'usuarios.views.index'),
+   
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^cambiar_clave', 'usuarios.views.cambiar_clave'),
-    url(r'^ConsultarDatosUsuario', 'usuarios.views.ConsultarDatosUsuario'),
-    url(r'^modificarDatosUsuario', 'usuarios.views.modificarDatosUsuario'),     
+    
     # Uncomment the next line to enable the admin:
     
     
@@ -30,11 +27,5 @@ urlpatterns = patterns('',
     url(r'^unidades/', include('unidades.urls')),
     url(r'^solicitudes/', include('solicitudes.urls')),
     
-)
-
-urlpatterns += patterns('unidades.views',
-    url(r'^solicitudPrivilegio$', 'solicitudPrivilegio'),
-    url(r'^otorgarPrivilegio$', 'otorgarPrivilegio'),
-    url(r'^registroUnidad$', 'registroUnidad'),
 )
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('usuarios.views',
+urlpatterns = patterns('',
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'usuarios/log_in.html'}),
     # Examples:
     # url(r'^crear_paso/$', 'crear_paso'),
     
 )
 
-urlpatterns += patterns('usuarios.views',
-    url(r'^cambiar_clave', 'cambiar_clave'),
-    url(r'^ConsultarDatosUsuario', 'ConsultarDatosUsuario'),
-    url(r'^modificarDatosUsuario', 'modificarDatosUsuario'),     
+urlpatterns += patterns('usuarios.views', 
+    url(r'registro/$', 'registro'),
+    url(r'logout/$', 'log_out'),
+    url(r'cambiar_clave/$', 'cambiar_clave'),
+    url(r'ConsultarDatosUsuario/$', 'consultar_datos_usuario'),
+    url(r'modificarDatosUsuario/$', 'modificar_datos_usuario'),     
 )
