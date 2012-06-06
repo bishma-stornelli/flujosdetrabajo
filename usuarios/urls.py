11a7b094728062deecc83ad7607010787e00b6b5
flujos <alemartini2204@gmail.com>
2012-06-05 23:30:53 -0430

    ajustado modulo de usuarios a convensiones y css completamente, ademas de uso adecuado de @login_require() en las vistas ... casos de uso : [Consulta de Perfil, modificar Perfil, Cambiar Clave, Registro de Usuario, Login]

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.views import logout, login
 
 
-urlpatterns = patterns('usuarios.views', 
-    url(r'^registro/$', 'registro'),
-    url(r'^login/', login, {'template_name':'usuarios/log_in.html'}),
-    url(r'^logout/$', logout, {'next_page': '/'}),
-    url(r'^cambiar_clave/$', 'cambiar_clave'),
-    url(r'^consultar_datos_usuario/$', 'consultar_datos_usuario'),
-    url(r'^modificar_datos_usuario/$', 'modificar_datos_usuario'),     
+urlpatterns = patterns('',
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'usuarios/log_in.html'}),)
+
+    # Examples:
+    # url(r'^crear_paso/$', 'crear_paso'),
+
+
+urlpatterns += patterns('usuarios.views', 
+    url(r'registro/$', 'registro'),
+    url(r'logout/$', 'log_out'),
+    url(r'logout/$', 'log_out'),
+    url(r'index/$', 'index'),
+    url(r'consultar_datos_usuario/$', 'consultar_datos_usuario'),
+    url(r'modificar_datos_usuario/$', 'modificar_datos_usuario'),
+    url(r'cambiar_clave/$', 'cambiar_clave'),
 )
 
