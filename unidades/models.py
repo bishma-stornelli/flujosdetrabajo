5870c1cc0ad0edaea7c17108774260c71dfb0e92
charles <chinocy@gmail.com>
2012-06-19 16:51:01 +0200

    a

--- unidades/models.py
+++ unidades/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import encoding
 #from unidades.groups import responsable
+#
+#
 
 class Unidad(models.Model):
     nombre = models.CharField(max_length=50)
