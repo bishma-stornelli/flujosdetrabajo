28fc0ee60e6acf9ebacc26fb3423aa6e9a3e7ca9
javier rodriguez <javierrodriguez9@gmail.com>
2012-06-23 14:34:11 -0430

    vista de listar solicitud

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template.context import RequestContext
+
+def listar_solicitudes(request):
+    # OBTENER PARAMETRO POR URL: /?unidad=X
+     return render_to_response('solicitudes/listar_solicitudes.html')
