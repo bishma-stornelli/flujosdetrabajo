d05c3b8f020c0cdd948054ae8ea7c08fd4860f23
Ale Paredes <aept7714@gmail.com>
2012-06-25 20:13:54 -0430

    Vistas para solicitudes

--- solicitudes/urls.py
+++ solicitudes/urls.py
@@ -7,8 +7,13 @@ urlpatterns = patterns('solicitudes.views',
     # Examples:
     # url(r'^crear_paso/$', 'crear_paso'),
 	url(r'^listar_solicitudes/$', 'listar_solicitudes'),
+    url(r'^crear_solicitud/$', 'crear_solicitud'),
     url(r'^index/$', TemplateView.as_view(template_name = 'solicitudes/base.html'), name='solicitudes_index'),
     url(r'^consultar_solicitud/(?P<solicitud_id>\d+)/$', 'consultar_solicitud'),
-    
+    url(r'^agregar_dato/$', 'agregar_dato'),
+    url(r'^completar_dato/$', 'completar_dato'),
+    url(r'^avanzar_solicitud/$', 'avanzar_solicitud'),
+    url(r'^retirar_solicitud/$', 'retirar_solicitud'),
+    url(r'^generar_informe/$', 'generar_informe')
 )
 
--- solicitudes/views.py
+++ solicitudes/views.py
@@ -14,3 +14,27 @@ def listar_solicitudes(request):
 @login_required
 def consultar_solicitud(request, solicitud_id):
      return render_to_response('solicitudes/consultar_solicitud.html')
+
+@login_required
+def crear_solicitud(request):
+     return render_to_response('solicitudes/crear_solicitud.html')
+
+@login_required
+def agregar_dato(request):
+     return render_to_response('solicitudes/agregar_dato.html')
+
+@login_required
+def completar_dato(request):
+     return render_to_response('solicitudes/completar_dato.html')
+
+@login_required
+def avanzar_solicitud(request):
+     return render_to_response('solicitudes/avanzar_solicitud.html')
+
+@login_required
+def retirar_solicitud(request):
+     return render_to_response('solicitudes/retirar_solicitud.html')
+
+@login_required
+def generar_informe(request):
+     return render_to_response('solicitudes/generar_informe.html')
