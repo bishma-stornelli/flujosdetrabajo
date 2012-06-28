fe654abcf4790acae5dfdd136ca6aaab98f83550
paulofabri <paulofabrigyp@gmail.com>
2012-06-28 16:10:51 -0430

    Mejora al listar solicitudes

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -7,13 +7,22 @@ from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from solicitudes.models import Solicitud
+from unidades.models import Unidad
+from flujos.models import Flujo
 
 @login_required
 def listar_solicitudes(request):
     #Obtiene las solicitudes en las que el usuario es el solicitante.
-    solicitudes = Solicitud.objects.filter(solicitantes=request.user)
+    solicitudes_solicitante = Solicitud.objects.filter(solicitantes=request.user).order_by('fecha_de_solicitud')
+    unidades = Unidad.objects.filter(miembros=request.user)
+    solicitudes_miembro=Solicitud.objects.none()
+    for u in unidades:
+        flujos= Flujo.objects.filter(unidad = u)
+        for f in flujos:
+            solicitudes_miembro= solicitudes_miembro | Solicitud.objects.filter(flujo=f)
+            solicitudes_miembro=solicitudes_miembro.order_by('fecha_de_solicitud')
     #Envia todas las solicitudes que coincidan a la vista. 
-    return render_to_response('solicitudes/listar_solicitudes.html', {'solicitudes':solicitudes},context_instance=RequestContext(request))
+    return render_to_response('solicitudes/listar_solicitudes.html', {'solicitudes_solicitante':solicitudes_solicitante, 'solicitudes_miembro':solicitudes_miembro},context_instance=RequestContext(request))
 
 @login_required
 def consultar_solicitud(request, solicitud_id):
